12d22ba9e174b36eda10ff78837eea6854674bce
Cephas Cardozo <95212186+cephascardozo@users.noreply.github.com>
2022-04-08 05:35:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#BMI CALCULATOR 2.0
+#BODY-MASS-INDEX Calculator
+#Created by Cephas Cardozo, 12/03/2022
+#Day 3 of 100DaysCodingChallenge
+
+print("BMI CALCULATOR")
+print("v2.0\n")
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+
+bmi = round(weight / height**2)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese!")
